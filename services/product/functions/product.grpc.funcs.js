a3aab415b040009137f002179e71db2012d213bd
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-12-29 13:12:00 +0330

    getting functions of proto file

---
+++ services/product/functions/product.grpc.funcs.js
@@ -0,0 +1,13 @@
+async function listOfProduct(call, callback) {}
+async function getProduct(call, callback) {}
+async function createProduct(call, callback) {}
+async function updateProduct(call, callback) {}
+async function deleteProduct(call, callback) {}
+
+module.exports = {
+  listOfProduct,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
