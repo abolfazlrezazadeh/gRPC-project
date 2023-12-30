153f5d62e01c8d09035af6c037ae8c46cdfd7031
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2023-12-30 13:58:40 +0330

    adding allRoutes to app.js

--- client/app.js
+++ client/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const { allRoutes } = require("./routes/index.routes");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(allRoutes)
 app.listen(4000,()=>{
     console.log('server running on port 4000');
 })
