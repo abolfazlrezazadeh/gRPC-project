60666f77483aa2971a04899b98b7ede934529a07
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-04 13:37:26 +0330

    adding blog.service anf grpcfunctions

---
+++ services/blog/blog.service.js
@@ -0,0 +1,13 @@
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const path = require("path");
+const protoPath = path.join(__dirname, "..", "..", "proto", "blog.proto");
+//load proto
+const blogProto = protoLoader.loadSync(protoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const {blogPackage} = grpc.loadPackageDefinition(blogProto);
---
+++ services/blog/functions/blog.grpc.funcs.js
@@ -0,0 +1,38 @@
+async function getAllBlogs(call, callback) {
+  try {
+  } catch (error) {
+    callback(error, null);
+  }
+}
+async function getBlog(call, callback) {
+  try {
+  } catch (error) {
+    callback(error, null);
+  }
+}
+async function createBlog(call, callback) {
+  try {
+  } catch (error) {
+    callback(error, null);
+  }
+}
+async function updateBlog(call, callback) {
+  try {
+  } catch (error) {
+    callback(error, null);
+  }
+}
+async function deleteBlog(call, callback) {
+  try {
+  } catch (error) {
+    callback(error, null);
+  }
+}
+
+module.exports = {
+  getAllBlogs,
+  getBlog,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+};
