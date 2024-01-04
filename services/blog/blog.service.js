43650735b21b6f0e9c233716cace7f0bfdc3a831
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-04 13:49:17 +0330

    completing of config of blog.service.js

--- services/blog/blog.service.js
+++ services/blog/blog.service.js
@@ -1,6 +1,14 @@
+const blogServerUrl = "localhost:4002";
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const path = require("path");
+const {
+  deleteBlog,
+  updateBlog,
+  createBlog,
+  getBlog,
+  getAllBlogs,
+} = require("./functions/blog.grpc.funcs");
 const protoPath = path.join(__dirname, "..", "..", "proto", "blog.proto");
 //load proto
 const blogProto = protoLoader.loadSync(protoPath, {
@@ -10,4 +18,31 @@ const blogProto = protoLoader.loadSync(protoPath, {
   defaults: true,
   oneofs: true,
 });
-const {blogPackage} = grpc.loadPackageDefinition(blogProto);
+const { blogPackage } = grpc.loadPackageDefinition(blogProto);
+
+function main() {
+  try {
+    const server = new grpc.Server();
+    server.addService(blogPackage.blogServices.service, {
+      getAllBlogs,
+      getBlog,
+      createBlog,
+      updateBlog,
+      deleteBlog,
+    });
+    // start server
+    server.bindAsync(
+      blogServerUrl,
+      grpc.ServerCredentials.createInsecure(),
+      (err, port) => {
+        if (err) return console.log(`blog.service.js error ${err.message}`);
+        console.log(`server running on port http://localhost:${port}`);
+        server.start();
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
